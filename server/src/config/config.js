ff3f8064b5b97ab760a21a769860de291fd2cf7a
Lcandres16 <116099973+Lcandres16@users.noreply.github.com>
2025-01-20 22:43:05 -0500

    add migration

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,10 +1,10 @@
 // src/config/config.js
 export const config = {
-    API_URL: 'http://localhost:3000',
-    endpoints: {
-      adminLogin: '/api/admin/login',
-      // Otros endpoints aquí
-    }
-  };
-  
-  export default config;
+  API_URL: "http://localhost:3000",
+  endpoints: {
+    adminLogin: "/api/admin/login",
+    // Otros endpoints aquí
+  },
+};
+
+export default config;
